5989ed3b513b3136bd4b025d12f9f925b9b59a80
Vuppalakavyasree <125962402+Vuppalakavyasree@users.noreply.github.com>
2024-11-18 11:14:15 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import TicketManagementApp from "./components/TicketManagementApp";
+
+function App() {
+  return (
+    <div className="App">
+      <TicketManagementApp />
+    </div>
+  );
+}
+
+export default App;
